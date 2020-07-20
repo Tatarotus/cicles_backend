229217aea6a8a0c8df9ce031164c241a8326b048
Samuel Dantas <sam@samuelresolve.com>
2020-07-20 11:27:16 -0300

    add api explanation

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,11 @@ const routes = express.Router();
 const angelController = require("./controllers/AngelController");
 
 //Main request
+routes.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to divine time API! Some routes: /planets - to check planets position, /time - to check current astrological time in the south hemisphere!"
+  });
+});
 routes.get("/planets", angelController.planets);
 routes.get("/time", angelController.time);
 
